b499cb41b06f6ff233b9fe79aa70150d6e63407b
Arash <arash382.jb@gmail.com>
2024-04-21 22:14:34 +0400

    chore: update @sentry/browser

--- dist/inject.js
+++ dist/inject.js
@@ -1,7 +1,6 @@
 import * as Sentry from "@sentry/browser";
 import { defaultOptions } from "./";
 window.Sentry = Sentry;
-console.log("__DEBUG__", __DEBUG__);
 Sentry.init({
     ...defaultOptions,
     // We replace this with true or false before injecting this code into the browser
